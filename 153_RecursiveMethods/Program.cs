a715dc022503932993bc9664aed3939040c53c60
Andres Sainz <andresmsainz@gmail.com>
2021-09-29 21:25:41 -0400

    initial commit

---
+++ 153_RecursiveMethods/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _153_RecursiveMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 5;
+
+            Console.WriteLine("Non recursive way");
+            Console.WriteLine(FactorialNonRecursive(number));
+
+            Console.WriteLine("Recursive way");
+            Console.WriteLine(Factorial(number));
+        }
+
+        //Non-Recursive way
+        public static int FactorialNonRecursive(int n)
+        {
+            if (n == 0)
+                return 1;
+            int value = 1;
+            for (int i = n; i > 0; i--)
+            {
+                value *= i;
+                Console.WriteLine(i);
+            }
+            return value;
+        }
+
+        //Recursive Way
+        public static int Factorial(int n)
+        {
+            if (n == 0) return 1;
+            return n * Factorial(n - 1);
+        }
+    }
+}
